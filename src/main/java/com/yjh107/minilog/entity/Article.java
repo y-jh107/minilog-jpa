9f1017282dc232eee5dbda26a35e83d2e025e779
양진혁 <jin99545@gmail.com>
2026-02-21 22:59:49 +0900

    feat: Article 엔티티 구현

--- src/main/java/com/yjh107/minilog/entity/Article.java
+++ src/main/java/com/yjh107/minilog/entity/Article.java
@@ -1,4 +1,45 @@
 package com.yjh107.minilog.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/*
+    테이블: articles
+    감사 기능
+    User 테이블과의 관계(author 필드): ManyToOne
+ */
+@Entity
+@Table(name = "articles")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
